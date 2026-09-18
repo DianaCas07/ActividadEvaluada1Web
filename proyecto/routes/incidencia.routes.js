20c2cbab5b6bca30929fbde30926b4b3311b9241
Kevin Pacheco <00210024@uca.edu.sv>
2026-09-18 12:10:22 -0600

    refactor: arreglar orden de complicacion de endpoints

--- proyecto/routes/incidencia.routes.js
+++ proyecto/routes/incidencia.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const{
     crearIncidencia,
     obtenerIncidencia,
+    listarIncidencia,
     filtrarIncidencia,
     eliminarIncidencia,
     obtenerEstadisticas,
@@ -13,14 +14,21 @@ const{
 // Punto 2: Registrar incidencia
 router.post('/', crearIncidencia);
 
+// Punto 7: Obtener estadisticas
+router.get('/estadisticas', obtenerEstadisticas);
+
+//Obtener incidencia por id
+router.get('/:id', obtenerIncidencia)
+
+//Obtener todas las incidencias
+router.get('/', listarIncidencia)
+
 // Punto 5: Cambiar estado
 router.put('/:id/estado', filtrarIncidencia);
 
 // Punto 6: Eliminar incidencia
 router.delete('/:id', eliminarIncidencia);
 
-// Punto 7: Obtener estadisticas
-router.get('/estadisticas', obtenerEstadisticas);
 
 // Punto 8: Clasificar incidencias
 router.get('/:id/clasificacion', clasificarIncidencias)
