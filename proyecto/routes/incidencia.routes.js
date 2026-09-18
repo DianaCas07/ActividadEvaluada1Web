a1f75e3d8a54173e7f83e1203f227089ac7d9732
Kevin Pleitez <00209923@uca.edu.sv>
2026-09-17 21:03:01 -0600

    :recycle: refactor: Actualizar incidencia.routes

--- proyecto/routes/incidencia.routes.js
+++ proyecto/routes/incidencia.routes.js
@@ -6,3 +6,11 @@ const{
     obtenerIncidencia,
     filtrarIncidencia
 } = require('../controllers/incidencias.Controller');
+
+// Punto 2: Registrar incidencia
+router.post('/', crearIncidencia);
+
+// Punto 5: Cambiar estado
+router.put('/:id/estado', filtrarIncidencia);
+
+module.exports = router;
