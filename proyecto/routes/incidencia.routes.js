6cca73d09e51a9dd69c6c4821a61b51d228e4198
DianaCas07 <00036625@uca.edu.sv>
2026-09-18 08:10:25 -0600

    :ambulance: se agregaron las rutas que se eliminaron de manera erronea

--- proyecto/routes/incidencia.routes.js
+++ proyecto/routes/incidencia.routes.js
@@ -5,10 +5,17 @@ const{
     crearIncidencia,
     obtenerIncidencia,
     filtrarIncidencia,
+    listarIncidencia,
     eliminarIncidencia,
     obtenerEstadisticas,
     clasificarIncidencias
 } = require('../controllers/incidencias.Controller');
+
+// punto 3: listar todas las incidencias
+router.get('/', listarIncidencia)
+
+//punto 4: obtener incidencia por ID
+router.get('/id', obtenerIncidencia)
 
 // Punto 2: Registrar incidencia
 router.post('/', crearIncidencia);
